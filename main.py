4d148fcd4300333e4ce80c2ef0afa61efb1d9aff
paul lubnin <egopaul@inbox.ru>
2022-12-28 11:27:15 +0500

    Described the main entities.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import requests
+import datetime
 from pprint import pprint
 
+import requests
 
-def get_vacancies(hh_url: str, language: str = 'Python', ):
 
+def get_vacancies(hh_url: str, date_from: datetime, date_to: datetime, language: str):
     payload = {
         'text': f'программист {language}',
         'area': 1,
-        'date_from': '2022-11-25',
-        'date_to': '2022-12-25'
+        'date_from': date_from.strftime('%Y-%m-%d'),
+        'date_to': date_to.strftime('%Y-%m-%d')
     }
     response = requests.get(hh_url, params=payload)
     response.raise_for_status()
@@ -16,8 +17,16 @@ def get_vacancies(hh_url: str, language: str = 'Python', ):
 
 
 if __name__ == '__main__':
+    today = datetime.datetime.today()
+    month_ago = today - datetime.timedelta(days=30)
     hh_url = 'https://api.hh.ru/vacancies'
     languages = ('JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go', 'Swift')
+    vacancies = {language: get_vacancies(hh_url, month_ago, today, language) for language in languages}
 
-    vacancies = {language: get_vacancies(hh_url, language).get("found") for language in languages}
-    pprint(vacancies)
+    python_jobs = vacancies['Python']['items']
+    python_salary = [job['salary'] for job in python_jobs]
+
+    pprint(python_salary)
+
+    # offer_count = {language: get_vacancies(hh_url, language).get("found") for language in languages}
+    # pprint(offer_count)
